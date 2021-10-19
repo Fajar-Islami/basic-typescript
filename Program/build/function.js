dda95b250376f96eba9d3124be5ede56d988b19b
Fajar-Islami <ahmadfajarislami@protonmail.com>
2021-10-19 21:05:12 +0700

    video 9 function

---
+++ Program/build/function.js
@@ -0,0 +1,13 @@
+"use strict";
+// Tipe data balikan function
+function getName() {
+    return "Hello, my name is fajar";
+}
+console.log(getName());
+function getAge() {
+    return 123;
+}
+function printName() {
+    console.log("Print name");
+}
+printName();
---
+++ Program/src/function.ts
@@ -0,0 +1,14 @@
+// Tipe data balikan function
+function getName(): string {
+  return "Hello, my name is fajar";
+}
+console.log(getName());
+
+function getAge(): number {
+  return 123;
+}
+
+function printName(): void {
+  console.log("Print name");
+}
+printName();
