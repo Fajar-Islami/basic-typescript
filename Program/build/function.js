a34be9a3872b3b27d3d539eb6092d9290e6833d7
Fajar-Islami <ahmadfajarislami@protonmail.com>
2021-10-19 21:09:14 +0700

    Video 9 - Argument pada function

--- Program/build/function.js
+++ Program/build/function.js
@@ -1,13 +1,19 @@
 "use strict";
-// Tipe data balikan function
-function getName() {
-    return "Hello, my name is fajar";
+// 1. Tipe data balikan function
+// function getName(): string {
+//   return "Hello, my name is fajar";
+// }
+// console.log(getName());
+// function getAge(): number {
+//   return 123;
+// }
+// function printName(): void {
+//   console.log("Print name");
+// }
+// printName();
+// 2. Function dengan Argument
+function multiply(val1, val2) {
+    return val1 * val2;
 }
-console.log(getName());
-function getAge() {
-    return 123;
-}
-function printName() {
-    console.log("Print name");
-}
-printName();
+var result = multiply(2, 20);
+console.log(result);
--- Program/src/function.ts
+++ Program/src/function.ts
@@ -1,14 +1,21 @@
-// Tipe data balikan function
-function getName(): string {
-  return "Hello, my name is fajar";
-}
-console.log(getName());
+// 1. Tipe data balikan function
+// function getName(): string {
+//   return "Hello, my name is fajar";
+// }
+// console.log(getName());
 
-function getAge(): number {
-  return 123;
-}
+// function getAge(): number {
+//   return 123;
+// }
 
-function printName(): void {
-  console.log("Print name");
+// function printName(): void {
+//   console.log("Print name");
+// }
+// printName();
+
+// 2. Function dengan Argument
+function multiply(val1: number, val2: number): number {
+  return val1 * val2;
 }
-printName();
+const result = multiply(2, 20);
+console.log(result);
