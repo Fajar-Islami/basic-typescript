7195a5d834cae6e23b9b55608c9a333542b6dc9f
Fajar-Islami <ahmadfajarislami@protonmail.com>
2021-10-19 20:47:01 +0700

    video 4 - type data any

--- Program/build/typeData.js
+++ Program/build/typeData.js
@@ -1,2 +1,21 @@
 "use strict";
-console.log("Hello");
+// 1. String
+// let nama = "Fajar"
+// nama = 12 // error
+// nama = true // error
+// nama = "Ahmad"
+//  2. Number
+// let umur: number = 25
+// umur = true // error
+// umur = [] // error
+// umur = 19
+//  3. Boolean
+// let isMarried: boolean
+// isMarried = true
+// isMarried = [] // error
+// isMarried = 19 // error
+// 4. Any ==> bisa diisi oleh banyak tipe data
+var heroes = "Iron man";
+heroes = 30;
+heroes = [];
+heroes = true;
