f47bb50eeca1743384c154ef9af7465c51873d0b
Fajar-Islami <ahmadfajarislami@protonmail.com>
2021-10-19 22:34:22 +0700

    video 16 - inheritance

--- Program/build/class.js
+++ Program/build/class.js
@@ -1,7 +1,23 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.User = void 0;
 var User = /** @class */ (function () {
+    //   name: string;
     //   constructor(name: string) {
     //     this.name = name;
     //   }
@@ -17,3 +33,26 @@ var User = /** @class */ (function () {
 exports.User = User;
 var user = new User("Ahmad Fajar", 17);
 console.log(user.name);
+/*  **note
+    public = bisa di akses disemua kelas / dari luas kelas
+    protected = hanya bisa diakses dari class tersebut dan kelas turunannya
+    private = hanya bisa diakses di akses class itu sendiri
+*/
+// Inheritance
+var Admin = /** @class */ (function (_super) {
+    __extends(Admin, _super);
+    function Admin() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.read = true;
+        _this.write = true;
+        return _this;
+    }
+    //   Method
+    Admin.prototype.getRole = function () {
+        return { read: this.read, write: this.write };
+    };
+    return Admin;
+}(User));
+var admin = new Admin("Ahmad", 27); // karena mewarisi user, maka diisi sesuai params di user
+console.log(admin);
+console.log(admin.getRole());
--- Program/src/class.ts
+++ Program/src/class.ts
@@ -1,4 +1,5 @@
 export class User {
+  //   name: string;
   //   constructor(name: string) {
   //     this.name = name;
   //   }
@@ -12,3 +13,27 @@ export class User {
 
 let user = new User("Ahmad Fajar", 17);
 console.log(user.name);
+
+/*  **note
+    public = bisa di akses disemua kelas / dari luas kelas
+    protected = hanya bisa diakses dari class tersebut dan kelas turunannya
+    private = hanya bisa diakses di akses class itu sendiri
+*/
+
+// Inheritance
+class Admin extends User {
+  read: boolean = true;
+  write: boolean = true;
+
+  //   Method
+  getRole(): {
+    read: boolean;
+    write: boolean;
+  } {
+    return { read: this.read, write: this.write };
+  }
+}
+
+let admin = new Admin("Ahmad", 27); // karena mewarisi user, maka diisi sesuai params di user
+console.log(admin);
+console.log(admin.getRole());
